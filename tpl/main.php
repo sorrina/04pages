29de7142e3cc81de0d9fc1e43ed0d11e9f6dd8da
sorrina <1981maariaa@gmail.com>
2016-02-28 15:27:45 +0100

    fertig
    Raffael, sei lieber nicht so genau, sind ein paar Notlösungen

--- tpl/main.php
+++ tpl/main.php
@@ -6,14 +6,12 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
   <link rel="stylesheet" href="stylesheets/style.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
-
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
 
   <nav class="nav-color">
     <div class="nav-wrapper">
-
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="?page=home">Startseite</a></li>
         <li><a href="?page=about">Über mich</a></li>
@@ -31,10 +29,10 @@
 
   <footer class="page-footer background">
       <div class="container">
-           <div class="row">
-
-               <i class="medium material-icons">phone</i>
-               <i class="medium material-icons">phone</i>
+           <div class="row text-center">
+               <a href="tel:0677 62023284"><i class="fa fa-mobile fa-5x"></i></a>
+               <a href="mailto:kathisag@hotmail.com"><i class="fa fa-envelope-o fa-5x"></i></a>
+               <a href="https://github.com/sorrina" target="blank"><i class="fa fa-github fa-5x"></i></a>
 
            </div>
        </div>
