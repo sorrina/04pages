249cc5173a162121806227505bb5c10f8ea9d17a
sorrina <1981maariaa@gmail.com>
2016-03-03 22:11:03 +0100

    so lange es noch funktioniert

--- admin/home.php
+++ admin/home.php
@@ -3,11 +3,26 @@
 
 
 <ul><?php
+/*
 foreach(glob("html/*.html") as $file){
     echo "<li>";
     echo "<a href='?admin=home&edit=$file'>$file</a>";
     echo"</li>";
+}*/
+if(isset($_POST["button"])){
+    //file_put_contents($_GET["edit"], $_POST["text"]);
+    $database->update("content", array("content" => $_POST["text"], "name" => $_POST["tabelle_name"]),array("id" => $_GET["edit"]));
 }
+
+
+
+$pages = $database->select("content", "*");
+foreach($pages as $page){
+    echo "<li>";
+    echo "<a href='?admin=home&edit=".$page["id"]."'>".$page["name"]."</a>";
+    echo "</li>";
+}
+
  ?>
 
 </ul>
@@ -16,17 +31,16 @@ foreach(glob("html/*.html") as $file){
 
 
 if(isset($_GET["edit"])){
-    if(isset($_POST["button"])){
-        file_put_contents($_GET["edit"], $_POST["text"]);
-    }
 
 
 
+$page = $database->get("content", array("content", "name"), array("id" => $_GET["edit"]));
     echo '<form action="" method="post">';
-        echo "<textarea name='text' id='editor1'>".file_get_contents($_GET["edit"])."</textarea>";
+        echo "<textarea name='text' id='editor1'>".$page["content"]."</textarea>";
+        //echo '<input type="text" name="tabelle_name" value="'.$page["name"].'">';
+        echo "<input type='text' name='tabelle_name' value=' ".$page['name']."'>";
         echo "<input type='submit' value='speichern' name='button'/>";
     echo"</form>";
-
 
 }
  ?>
